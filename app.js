0347647e273ebc56f88dd6773635bc71896b0377
markovecko <marko.vecko@gmail.com>
2017-01-10 00:37:30 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
 .directive('foundItems', FoundItems)
 .service('MenuSearchService', MenuSearchService)
-.constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
 
 function FoundItems() {
   var ddo = {
